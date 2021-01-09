d4bf4902566962f6825d9ec22981948f9d8be2a5
truong.hd154023 <truong.hd154023@sis.hust.edu.vn>
2021-01-09 16:37:22 +0700

    new filePath

--- get_mp3_header_9_1/Program.cs
+++ get_mp3_header_9_1/Program.cs
@@ -9,7 +9,8 @@ namespace get_mp3_header_9_1
         {
             Console.WriteLine("Hello World!");
 
-            string _filePath = @"E:\truyenthanhproject\read_mp3\read_mp3\LoveIsBlue.mp3";
+            //string _filePath = @"E:\truyenthanhproject\read_mp3\read_mp3\LoveIsBlue.mp3";
+            string _filePath = "LoveIsBlue.mp3"; //in server
 
             byte[] buff = File.ReadAllBytes(_filePath);
             int buff_Length = buff.Length;
