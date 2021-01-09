85843729be0439f3b2900675f2b48aee6d86e8f1
truong.hd154023 <truong.hd154023@sis.hust.edu.vn>
2021-01-09 22:21:57 +0700

    total time

--- get_mp3_header_9_1/MP3_header.cs
+++ get_mp3_header_9_1/MP3_header.cs
@@ -8,44 +8,114 @@ namespace get_mp3_header_9_1
     {
         //mp3 is MPEG 1 Layer III or MPEG 2 layer III, detail: https://en.wikipedia.org/wiki/MP3
 
-        //mp3 header include 32-bit, consider 32-bit as little edian
+        //mp3 header include 32-bit
         // byte0    byte1   byte2   byte3
-        //bit 0                         31
+        //bit 31                        0
         //detail: http://www.mp3-tech.org/programmer/frame_header.
 
-        //bit 0-10 is frame sync, all bit is 1, (byte0 == FF) && (byte1 & 0xE0 == 0xE0)
+        //bit 31-21 is frame sync, all bit is 1, (byte0 == FF) && (byte1 & 0xE0 == 0xE0)
 
-        //bit 11-12 : MPEG version, 11: V1, 10: V2
+        //bit 20-19 : MPEG version, 11: V1, 10: V2
         int version;
 
         public int Version { get => version; }
 
-        //bit 13-14 Layer, just consider layer III, 01
+        //bit 18-17 Layer, just consider layer III, 01
         const int layer = 3;
         public static int Layer => layer;
 
-        
 
-        //bit 15, protected bit, don't count
 
-        //bit 16-19 bitrate
-        static readonly int?[] bitrate_V1_L3 = { null, 32, 40, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, null }; // MPEG 1, layer III
-        static readonly int?[] bitrate_V2_L3 = { null, 8, 16, 24, 32, 40, 48, 56, 64, 80, 96, 112, 128, 144, 160, null }; // MPEG 2, layer III
+        //bit 16, protected bit, don't count
+
+        //bit 15-12 bitrate
+        static readonly int[] bitrate_V1_L3 = { 0, 32, 40, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, 0 }; // MPEG 1, layer III
+        static readonly int[] bitrate_V2_L3 = { 0, 8, 16, 24, 32, 40, 48, 56, 64, 80, 96, 112, 128, 144, 160, 0 }; // MPEG 2, layer III
         int bitrate;
         public int Bitrate { get => bitrate; }
 
 
-        //bit 20-21 sample rate
-        static readonly int?[] sample_rate_V1 = { 44100, 48000, 32000, null };
-        static readonly int?[] sample_rate_V2 = { 22050, 24000, 16000, null };
+        //bit 11-10 sample rate
+        static readonly int[] sample_rate_V1 = { 44100, 48000, 32000, 0 };
+        static readonly int[] sample_rate_V2 = { 22050, 24000, 16000, 0 };
         int sample_rate;
         public int Sample_rate { get => sample_rate; }
 
-        //bit 22, padding bit
+        //sample per frame
+        int sample_per_frame;
+        static readonly int[] sample_per_frame_version = { 1152, 576 };
+
+        public int Sample_per_frame { get => sample_per_frame; }
+
+        //bit 9, padding bit
         int padding;
         public int Padding { get => padding; }
-
         
+        int frame_size;
+        public int Frame_size { get => frame_size; }
 
+        public bool IsValidHeader(bool first_header, MP3_header MP3_header_first, byte[] buff, int i_buff, int buff_length)
+        {
+            //get infor header
+            int header = (int)buff[i_buff + 3] | ((int)buff[i_buff + 2] << 8) | ((int)buff[i_buff + 1] << 16) | ((int)buff[i_buff] << 24);
+
+            //get version
+            int tmp = (header >> 19) & 0b11;
+            if (tmp == 0b11)
+                version = 1;
+            else if (tmp == 0b10)
+                version = 2;
+            else
+                return false;
+
+            //get layer
+            tmp = (header >> 17) & 0b11;
+            if (tmp != 0b01) //layer III
+                return false;
+
+            //get bitrate
+            tmp = (header >> 12) & 0b1111;
+            if ((tmp == 0) || (tmp == 0b1111))
+                return false;
+            if (version == 1)
+                bitrate = bitrate_V1_L3[tmp];
+            else if (version == 2)
+                bitrate = bitrate_V2_L3[tmp];
+
+            //get smaple rate
+            tmp = (header >> 10) & 0b11;
+            if (tmp == 0b11)
+                return false;
+            if (version == 1)
+                sample_rate = sample_rate_V1[tmp];
+            else if (version == 2)
+                sample_rate = sample_rate_V2[tmp];
+
+            if (first_header)
+            {
+                if ((MP3_header_first.version != version) || (MP3_header_first.sample_rate != sample_rate))
+                    return false;
+            }
+
+            //get padding
+            padding = (header >> 9) & 1;
+
+            //get sample per frame
+            sample_per_frame = sample_per_frame_version[version - 1];
+
+            //get frame size
+            double frame_size_tmp = bitrate * 1000 * sample_per_frame / 8 / sample_rate + padding;
+            frame_size = (int)frame_size_tmp;
+
+            //check next frame
+            if ((i_buff + frame_size - 1) < buff_length)
+            {
+                //i_buff += (frame_size - 1);
+            }
+            else
+                return false;
+
+            return true;
+        }
     }
 }
--- get_mp3_header_9_1/Program.cs
+++ get_mp3_header_9_1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 
 namespace get_mp3_header_9_1
@@ -9,22 +10,71 @@ namespace get_mp3_header_9_1
         {
             Console.WriteLine("Hello World!");
 
-            //string _filePath = @"E:\truyenthanhproject\read_mp3\read_mp3\LoveIsBlue.mp3";
-            string _filePath = "LoveIsBlue.mp3"; //in server
+            string _filePath = @"E:\truyenthanhproject\read_mp3\Binz22.mp3";
+            //string _filePath = @"E:\truyenthanhproject\read_mp3\LoveIsBlue.mp3";
+            //string _filePath = "LoveIsBlue.mp3"; //in server
+
+            byte[] send_buff = new byte[1500];
 
             byte[] buff = File.ReadAllBytes(_filePath);
             int buff_Length = buff.Length;
 
-            int i_buff = 0;
+            int i_buff = 0, header_count = 0, padding = 0; ;
 
-            while (i_buff < buff_Length - 3)
+            var MP3_header_first = new MP3_header();
+
+            var MP3_header_next = new MP3_header();
+
+            //create timer
+            Stopwatch stopWatch;
+            stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            //get first header
+            while (i_buff < buff_Length - 4)
             {
-
+                if((buff[i_buff] == 0xFF) &&((buff[i_buff + 1] & 0xE0) == 0xE0)) //sync bit
+                {
+                    if (MP3_header_first.IsValidHeader(false, null, buff, i_buff, buff_Length))
+                    {
+                        header_count++;
+                        padding += MP3_header_first.Padding;
+                        //buff.CopyTo()
+                        Buffer.BlockCopy(buff, i_buff, send_buff, 0, MP3_header_first.Frame_size);
+                        i_buff += MP3_header_first.Frame_size;
+                        break;
+                    }
+                        
+                }
+                    
                 i_buff++;
             }
 
-            Console.WriteLine(i_buff);
+            //get next header
+            while (i_buff < buff_Length - 4)
+            {
+                if ((buff[i_buff] == 0xFF) && ((buff[i_buff + 1] & 0xE0) == 0xE0)) //sync bit
+                {
+                    if(MP3_header_next.IsValidHeader(true, MP3_header_first, buff, i_buff, buff_Length))
+                    {
+                        header_count++;
+                        padding += MP3_header_next.Padding;
+                        Buffer.BlockCopy(buff, i_buff, send_buff, 0, MP3_header_next.Frame_size);
+                        i_buff += MP3_header_next.Frame_size - 1; //because i_buff++ after
+                        //i_buff += MP3_header_first.Frame_size; continue;
+                    }
+                }
+                    
+                i_buff++;
+            }
 
+            Console.WriteLine("Total frame: {0}", header_count);
+
+            Console.WriteLine("Padding frame: {0}", padding);
+
+            double endTime = stopWatch.Elapsed.TotalMilliseconds;
+
+            Console.WriteLine("Total time: {0} ms", endTime);
             Console.ReadLine();
         }
     }
