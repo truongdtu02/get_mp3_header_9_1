9def9f295d39887aa546c8ac29e6f4459144539e
truong.hd154023 <truong.hd154023@sis.hust.edu.vn>
2021-01-11 15:22:53 +0700

    split frame -> packet

--- get_mp3_header_9_1/MP3_header.cs
+++ get_mp3_header_9_1/MP3_header.cs
@@ -6,6 +6,13 @@ namespace get_mp3_header_9_1
 {
     class MP3_header
     {
+        //constructor initialize
+        byte[] mp3_buff;
+        int mp3_buff_length;
+        bool IsValidMP3_bool = false;
+        public byte[] Mp3_buff { get => mp3_buff; }
+        public int Mp3_buff_length { get => mp3_buff_length; }
+
         //mp3 is MPEG 1 Layer III or MPEG 2 layer III, detail: https://en.wikipedia.org/wiki/MP3
 
         //mp3 header include 32-bit
@@ -16,7 +23,7 @@ namespace get_mp3_header_9_1
         //bit 31-21 is frame sync, all bit is 1, (byte0 == FF) && (byte1 & 0xE0 == 0xE0)
 
         //bit 20-19 : MPEG version, 11: V1, 10: V2
-        int version;
+        int version, version_first_header;
 
         public int Version { get => version; }
 
@@ -38,7 +45,7 @@ namespace get_mp3_header_9_1
         //bit 11-10 sample rate
         static readonly int[] sample_rate_V1 = { 44100, 48000, 32000, 0 };
         static readonly int[] sample_rate_V2 = { 22050, 24000, 16000, 0 };
-        int sample_rate;
+        int sample_rate, sample_rate_first_header;
         public int Sample_rate { get => sample_rate; }
 
         //sample per frame
@@ -50,11 +57,17 @@ namespace get_mp3_header_9_1
         //bit 9, padding bit
         int padding;
         public int Padding { get => padding; }
-        
-        int frame_size;
+
+        int frame_size = 0;
         public int Frame_size { get => frame_size; }
 
-        public bool IsValidHeader(bool first_header, MP3_header MP3_header_first, byte[] buff, int i_buff, int buff_length)
+        int start_frame = 0, end_frame = 0;
+        public int Start_frame { get => start_frame; }
+        public int End_frame { get => end_frame; }
+
+        
+
+        public bool IsValidHeader_old(bool first_header, MP3_header MP3_header_first, byte[] buff, int i_buff, int buff_length)
         {
             //get infor header
             int header = (int)buff[i_buff + 3] | ((int)buff[i_buff + 2] << 8) | ((int)buff[i_buff + 1] << 16) | ((int)buff[i_buff] << 24);
@@ -117,5 +130,134 @@ namespace get_mp3_header_9_1
 
             return true;
         }
+
+        public bool IsValidHeader(byte[] buff, int i_buff, int buff_length)
+        {
+            //get infor header
+            int header = (int)buff[i_buff + 3] | ((int)buff[i_buff + 2] << 8) | ((int)buff[i_buff + 1] << 16) | ((int)buff[i_buff] << 24);
+
+            //get version
+            int tmp = (header >> 19) & 0b11;
+            if (tmp == 0b11)
+                version = 1;
+            else if (tmp == 0b10)
+                version = 2;
+            else
+                return false;
+
+            //get layer
+            tmp = (header >> 17) & 0b11;
+            if (tmp != 0b01) //layer III
+                return false;
+
+            //get bitrate
+            tmp = (header >> 12) & 0b1111;
+            if ((tmp == 0) || (tmp == 0b1111))
+                return false;
+            if (version == 1)
+                bitrate = bitrate_V1_L3[tmp];
+            else if (version == 2)
+                bitrate = bitrate_V2_L3[tmp];
+
+            //get smaple rate
+            tmp = (header >> 10) & 0b11;
+            if (tmp == 0b11)
+                return false;
+            if (version == 1)
+                sample_rate = sample_rate_V1[tmp];
+            else if (version == 2)
+                sample_rate = sample_rate_V2[tmp];
+
+            //check, if it is next frame, compare with first frame
+            if (!IsValidMP3_bool) //it is first frame
+            {
+                version_first_header = version;
+                sample_rate_first_header = sample_rate;
+            }
+            else //next frame
+            {
+                if ((version_first_header != version) || (sample_rate_first_header != sample_rate))
+                    return false;
+            }
+
+            //get padding
+            padding = (header >> 9) & 1;
+
+            //get sample per frame
+            sample_per_frame = sample_per_frame_version[version - 1];
+
+            //get frame size
+            double frame_size_tmp = bitrate * 1000 * sample_per_frame / 8 / sample_rate + padding;
+            frame_size = (int)frame_size_tmp;
+
+            //check next frame
+            if ((i_buff + frame_size) > buff_length) //out of range
+            {
+                return false;
+            }
+            start_frame = i_buff;
+            return true;
+        }
+
+        public bool IsValidMp3()
+        {
+            if((mp3_buff == null) || (mp3_buff_length < 1)) //invalid initialize
+            {
+                return false;
+            }
+            int index_buff_mp3 = 0;
+            while (index_buff_mp3 < (mp3_buff_length - 3)) // a frame has at least 4 bytes
+            {
+                if ((mp3_buff[index_buff_mp3] == 0xFF) && ((mp3_buff[index_buff_mp3 + 1] & 0xE0) == 0xE0)) //sync bit
+                {
+                    if (IsValidHeader(mp3_buff, index_buff_mp3, mp3_buff_length))
+                    {
+                        IsValidMP3_bool = true;
+                        //reset frame_size, because in IsValidHeader, it change frame_size, but we just want check,
+                        // not get header infor
+                        frame_size = 0;
+                        return true;
+                    }
+                }
+                index_buff_mp3++;
+            }
+            return false;
+        }
+
+        public bool ReadNextFrame()
+        {
+            if(!IsValidMP3_bool) //wrong mp3
+            {
+                return false;
+            }
+
+            int index_buff_mp3 = start_frame + frame_size;
+
+            while (index_buff_mp3 < (mp3_buff_length - 3)) // a frame has at least 4 bytes
+            {
+                if ((mp3_buff[index_buff_mp3] == 0xFF) && ((mp3_buff[index_buff_mp3 + 1] & 0xE0) == 0xE0)) //sync bit
+                {
+                    if (IsValidHeader(mp3_buff, index_buff_mp3, mp3_buff_length))
+                    {
+                        return true;
+                    }
+                }
+                index_buff_mp3++;
+            }
+            return false;
+        }
+
+        //constructor
+        public MP3_header(byte[] _Mp3_buff, int _Mp3_buff_length)
+        {
+            mp3_buff = _Mp3_buff;
+            mp3_buff_length = _Mp3_buff_length;
+        }
+        public MP3_header()
+        {
+            mp3_buff = null;
+            mp3_buff_length = 0;
+        }
     }
 }
+
--- get_mp3_header_9_1/Program.cs
+++ get_mp3_header_9_1/Program.cs
@@ -6,73 +6,141 @@ namespace get_mp3_header_9_1
 {
     class Program
     {
+        static bool left_frame_not_packet = false;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
-            string _filePath = "Binz22.mp3";
+            //string _filePath = "Binz22.mp3";
             //string _filePath = @"E:\truyenthanhproject\read_mp3\Binz22.mp3";
             //string _filePath = @"E:\truyenthanhproject\read_mp3\LoveIsBlue.mp3";
             //string _filePath = "LoveIsBlue.mp3"; //in server
+            string _filePath = @"E:\truyenthanhproject\read_mp3\Binz2.mp3";
+            //                                                    CRC   ethnernet   IPv4   UDP header
+            const int Max_send_buff_length = 1472; //MTU = 1518 - 4 -     14 -      20 -      8  
 
-            byte[] send_buff = new byte[1500];
-
+            byte[] send_buff = new byte[Max_send_buff_length];
+            
             byte[] buff = File.ReadAllBytes(_filePath);
             int buff_Length = buff.Length;
 
-            int i_buff = 0, header_count = 0, padding = 0; ;
+            int header_count = 0, padding = 0;
 
-            var MP3_header_first = new MP3_header();
+            double ave_bitrate = 0;
 
-            var MP3_header_next = new MP3_header();
+            
 
             //create timer
             Stopwatch stopWatch;
             stopWatch = new Stopwatch();
             stopWatch.Start();
 
+            var mp3_reader = new MP3_header(buff, buff_Length);
+            mp3_reader.IsValidMp3();
+
+            int numOfFrame = 1;
+            while(numOfFrame > 0)
+                numOfFrame = packet_udp_frameMP3(send_buff, Max_send_buff_length, mp3_reader);
+
             //get first header
-            while (i_buff < buff_Length - 4)
-            {
-                if ((buff[i_buff] == 0xFF) && ((buff[i_buff + 1] & 0xE0) == 0xE0)) //sync bit
-                {
-                    //check it is first time
-                    if (MP3_header_first.Version == 0) //null
-                    {
-                        if (MP3_header_first.IsValidHeader(false, null, buff, i_buff, buff_Length))
-                        {
-                            header_count++;
-                            padding += MP3_header_first.Padding;
-                            //buff.CopyTo()
-                            Buffer.BlockCopy(buff, i_buff, send_buff, 0, MP3_header_first.Frame_size);
-                            i_buff += MP3_header_first.Frame_size - 1;
-                        }
-                    }
-                    else //next frame
-                    {
-                        if (MP3_header_next.IsValidHeader(true, MP3_header_first, buff, i_buff, buff_Length))
-                        {
-                            header_count++;
-                            padding += MP3_header_next.Padding;
-                            Buffer.BlockCopy(buff, i_buff, send_buff, 0, MP3_header_next.Frame_size);
-                            i_buff += MP3_header_next.Frame_size - 1; //because i_buff++ after
-                                                                      //i_buff += MP3_header_first.Frame_size; continue;
-                        }
-                    }
+            //while (i_buff < buff_Length - 4)
+            //{
+            //    if ((buff[i_buff] == 0xFF) && ((buff[i_buff + 1] & 0xE0) == 0xE0)) //sync bit
+            //    {
+            //        //check it is first time
+            //        if (MP3_header_first.Version == 0) //null
+            //        {
+            //            if (MP3_header_first.IsValidHeader(false, null, buff, i_buff, buff_Length))
+            //            {
+            //                header_count++;
+            //                padding += MP3_header_first.Padding;
+            //                //buff.CopyTo()
+            //                //Buffer.BlockCopy(buff, i_buff, send_buff, 0, MP3_header_first.Frame_size);
+            //                beggin_index_frame = i_buff;
+            //                offset_frame = MP3_header_first.Frame_size;
 
-                }
+            //                i_buff += MP3_header_first.Frame_size - 1;
+            //            }
+            //        }
+            //        else //next frame
+            //        {
+            //            if (MP3_header_next.IsValidHeader(true, MP3_header_first, buff, i_buff, buff_Length))
+            //            {
+            //                header_count++;
+            //                padding += MP3_header_next.Padding;
+            //                //Buffer.BlockCopy(buff, i_buff, send_buff, 0, MP3_header_next.Frame_size);
+            //                beggin_index_frame = i_buff;
+            //                offset_frame = MP3_header_next.Frame_size;
+            //                i_buff += MP3_header_next.Frame_size - 1; //because i_buff++ after
+            //                                                          //i_buff += MP3_header_first.Frame_size; continue;
+            //            }
+            //        }
 
-                i_buff++;
-            }
+            //    }               
+            //    i_buff++;
+
+            //    //if offset_frame > 0, mean that found a frame
+            //    //packet frame by beggin_index_frame, offset_frame
+            //    //reset offset_frame = 0
+            //    //send 
+            //}
 
             Console.WriteLine("Total frame: {0}", header_count);
 
             Console.WriteLine("Padding frame: {0}", padding);
 
+            if(header_count > 0)
+            {
+                Console.WriteLine("Average bitrate: {0}", ave_bitrate / header_count);
+            }           
+
             double endTime = stopWatch.Elapsed.TotalMilliseconds;
 
             Console.WriteLine("Total time: {0} ms", endTime);
             Console.ReadLine();
+        }
+
+        static public int packet_udp_frameMP3(byte[] _send_buff, int _max_send_buff_length, MP3_header _mp3_reader)
+        {
+            //packet: 4-byte
+            int numOfFrame = 0, totalLength = 0;
+            while (true)
+            {
+                if(left_frame_not_packet)
+                {
+                    left_frame_not_packet = false;
+                }
+                else if(_mp3_reader.ReadNextFrame())
+                {
+
+                }
+                else
+                {
+                    break;
+                }
+                //check space for cmemcpy //(4+4) for numOfFrame, totalLength
+                if (_mp3_reader.Frame_size <= (_max_send_buff_length - (4+4) - totalLength))
+                {
+                    Buffer.BlockCopy(_mp3_reader.Mp3_buff, _mp3_reader.Start_frame, _send_buff,(4+4+totalLength), _mp3_reader.Frame_size);
+                    totalLength += _mp3_reader.Frame_size;
+                    numOfFrame++;
+                }
+                else
+                {
+                    left_frame_not_packet = true;
+                    break;
+                }
+            }
+            //copy num of frame (little edian)
+            byte[] tmp_byte = BitConverter.GetBytes(numOfFrame);
+            Buffer.BlockCopy(tmp_byte, 0, _send_buff, 0, 4);
+
+            //copy total length frame (byte) (little edian)
+            tmp_byte = BitConverter.GetBytes(totalLength);
+            Buffer.BlockCopy(tmp_byte, 0, _send_buff, 4, 4);
+
+            return numOfFrame;
         }
     }
 }
